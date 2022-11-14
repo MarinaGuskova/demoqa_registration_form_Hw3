4fe808fb82e4f596843e08dfb0fe42828dca97a5
Marina Guskova <marinaguskova08@gmail.com>
2022-11-14 21:12:25 +0300

    Test completed

--- src/test/java/RegistrationFormTests.java
+++ src/test/java/RegistrationFormTests.java
@@ -1,6 +1,5 @@
 package com.example.demoqa;
 
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,35 +7,68 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormTests {
     @BeforeAll
+
     static void BeforeAll() {
         Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1132x753";
-//        Configuration.startMaximized = true;
+        Configuration.browserSize = "1620x800";
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.browser = "firefox";
+
     }
     @Test
     void FillFormTest(){
 
       open("/automation-practice-form");
 
+      //  Кнопка закрытия баннера не сработала $("#close-fixedban").click();
+      // убрать баннер
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
       $("#firstName").setValue("Собака");
       $("#lastName").setValue("Сутулая");
       $("#userEmail").setValue("Sutulaya@mail.net");
-      $x("//div[@class='custom-control custom-radio custom-control-inline'][3]").click();
+      $x("//label[text()='Other']").click();
       $("#userNumber").setValue("1234567890");
       $("#dateOfBirthInput").click();
       $(".react-datepicker__month-select").click();
       $(".react-datepicker__month-select").selectOption("February");
       $(".react-datepicker__year-select").click();
       $(".react-datepicker__year-select").selectOption("2098");
-      $(".react-datepicker__year-select").click();
-      $x("//div[@class='react-datepicker__week'][5]//div[@class='react-datepicker__day react-datepicker__day--028']").click();
-      $("#close-fixedban").click();
+      $x("//div[@aria-label='Choose Friday, February 28th, 2098']").click();
+      $x("//input[@id='subjectsInput']").setValue("Офигенный автотест");
+      $x("//label[text()='Sports']").click();
+      $x("//label[text()='Reading']").click();
+      $x("//label[text()='Music']").click();
+      //     Загрузить файл
+      $x("//input[@id='uploadPicture']").uploadFile(new File("src/test/resources/dog.jfif"));
+      $x("//textarea[@id='currentAddress']").setValue("Ramdev Mandir");
+      $x("//div[@id='state']").click();
+      $x("//div[text()='Rajasthan']").click();
+      $x("//div[@id='city']").click();
+      $x("//div[text()='Jaiselmer']").click();
+
+      $("#submit").click();
+
+
+      $x("//div[text()='Thanks for submitting the form']").shouldBe(Condition.visible);
+      $(".table-responsive").shouldHave(text("Собака Сутулая"),
+                                                 text("Sutulaya@mail.net"),
+                                                 text("Other"),
+                                                 text("1234567890"),
+                                                 text("28 February,2098"),
+                                                 text("Sports, Reading, Music"),
+                                                 text("dog.jfif"),
+                                                 text("Ramdev Mandir"),
+                                                 text("Rajasthan Jaiselmer")
+      );
+
+
     }
 
 }
