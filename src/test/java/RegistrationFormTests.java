fd004ca44cb9850898b89e6485161732ed521688
Marina Guskova <marinaguskova08@gmail.com>
2022-11-15 19:19:03 +0300

    Cltl+Alt+L

--- src/test/java/RegistrationFormTests.java
+++ src/test/java/RegistrationFormTests.java
@@ -20,52 +20,53 @@ public class RegistrationFormTests {
 //        Configuration.browser = "firefox";
 
     }
+
     @Test
-    void FillFormTest(){
+    void FillFormTest() {
 
-      open("/automation-practice-form");
+        open("/automation-practice-form");
 
-      //  Кнопка закрытия баннера не сработала $("#close-fixedban").click();
-      // убрать баннер
+        //  Кнопка закрытия баннера не сработала $("#close-fixedban").click();
+        // убрать баннер
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
 
-      $("#firstName").setValue("Собака");
-      $("#lastName").setValue("Сутулая");
-      $("#userEmail").setValue("Sutulaya@mail.net");
-      $x("//label[text()='Other']").click();
-      $("#userNumber").setValue("1234567890");
-      $("#dateOfBirthInput").click();
-      $(".react-datepicker__month-select").click();
-      $(".react-datepicker__month-select").selectOption("February");
-      $(".react-datepicker__year-select").click();
-      $(".react-datepicker__year-select").selectOption("2098");
-      $x("//div[@aria-label='Choose Friday, February 28th, 2098']").click();
-      $x("//input[@id='subjectsInput']").setValue("Офигенный автотест");
-      $x("//label[text()='Sports']").click();
-      $x("//label[text()='Reading']").click();
-      $x("//label[text()='Music']").click();
-      //     Загрузить файл
-      $x("//input[@id='uploadPicture']").uploadFile(new File("src/test/resources/dog.jfif"));
-      $x("//textarea[@id='currentAddress']").setValue("Ramdev Mandir");
-      $x("//div[@id='state']").click();
-      $x("//div[text()='Rajasthan']").click();
-      $x("//div[@id='city']").click();
-      $x("//div[text()='Jaiselmer']").click();
+        $("#firstName").setValue("Собака");
+        $("#lastName").setValue("Сутулая");
+        $("#userEmail").setValue("Sutulaya@mail.net");
+        $x("//label[text()='Other']").click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("2098");
+        $x("//div[@aria-label='Choose Friday, February 28th, 2098']").click();
+        $x("//input[@id='subjectsInput']").setValue("Офигенный автотест");
+        $x("//label[text()='Sports']").click();
+        $x("//label[text()='Reading']").click();
+        $x("//label[text()='Music']").click();
+        //     Загрузить файл
+        $x("//input[@id='uploadPicture']").uploadFile(new File("src/test/resources/dog.jfif"));
+        $x("//textarea[@id='currentAddress']").setValue("Ramdev Mandir");
+        $x("//div[@id='state']").click();
+        $x("//div[text()='Rajasthan']").click();
+        $x("//div[@id='city']").click();
+        $x("//div[text()='Jaiselmer']").click();
 
-      $("#submit").click();
+        $("#submit").click();
 
 
-      $x("//div[text()='Thanks for submitting the form']").shouldBe(Condition.visible);
-      $(".table-responsive").shouldHave(text("Собака Сутулая"),
-                                        text("Sutulaya@mail.net"),
-                                        text("Other"),
-                                        text("1234567890"),
-                                        text("28 February,2098"),
-                                        text("Sports, Reading, Music"),
-                                        text("dog.jfif"),
-                                        text("Ramdev Mandir"),
-                                        text("Rajasthan Jaiselmer")
-      );
+        $x("//div[text()='Thanks for submitting the form']").shouldBe(Condition.visible);
+        $(".table-responsive").shouldHave(text("Собака Сутулая"),
+                text("Sutulaya@mail.net"),
+                text("Other"),
+                text("1234567890"),
+                text("28 February,2098"),
+                text("Sports, Reading, Music"),
+                text("dog.jfif"),
+                text("Ramdev Mandir"),
+                text("Rajasthan Jaiselmer")
+        );
     }
 }
